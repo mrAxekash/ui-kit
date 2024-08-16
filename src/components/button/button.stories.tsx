a6181d13390696b42c321dfc3091a032717af1b1
mrAxekash <aliaxandr_ku@mail.ru>
2024-08-16 23:37:17 +0300

    create polymorfic component

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -21,3 +21,21 @@ export const AlertOnClick = {
     onClick: () => alert('Button clicked Alert'),
   },
 }
+
+export const FullWidth = {
+  args: {
+    children: 'Alert',
+    onClick: () => alert('Button clicked Alert'),
+    fullWidth: true,
+  },
+}
+
+export const ButtonAsLink = {
+  args: {
+    variant: 'primary',
+    as: 'a',
+    children: 'Button as link',
+    href: 'https://google.com',
+    target: '_blank',
+  },
+}
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,23 @@
-import {ComponentPropsWithoutRef} from 'react'
+import { ComponentPropsWithoutRef, ElementType } from 'react'
 import s from './button.module.scss'
-import clsx from "clsx";
+import clsx from 'clsx'
 
-export type ButtonProps = {
-  variant: 'primary' | 'secondary' | 'outlined';
-} & ComponentPropsWithoutRef<'button'>;
+export type ButtonProps<T extends ElementType = 'button'> = {
+  variant: 'primary' | 'secondary' | 'outlined' | 'text' | 'link'
+  fullWidth?: boolean
+  as?: T
+} & ComponentPropsWithoutRef<T>
 
-export const Button = ({ className, variant, ...rest }: ButtonProps) => {
-  return <button {...rest} className={clsx(s[variant], className)}  />
+export function Button<T extends ElementType = 'button'>({
+  className,
+  fullWidth = false,
+  variant = 'primary',
+  as,
+  ...rest
+}: ButtonProps<T>) {
+  const Component = as ?? 'button'
+
+  return (
+    <Component {...rest} className={clsx(s[variant], className ?? '', fullWidth && s.fullWidth)} />
+  )
 }
