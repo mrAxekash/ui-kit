03f35edda64ae46598b2da3ddc11bdb0929e42f9
mrAxekash <aliaxandr_ku@mail.ru>
2024-08-21 21:31:19 +0300

    fix bug with build project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-import * as path from 'path'
+import { devDependencies, peerDependencies } from './package.json'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(peerDependencies),
+        ...Object.keys(devDependencies),
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
   },
 })
